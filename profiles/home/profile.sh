be78d02b1a4b69d99cc4153fd310828b451efe7d
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2018-11-04 22:36:16 +0100

    Renamed thuis to home

--- profiles/home/profile.sh
+++ profiles/home/profile.sh
@@ -6,9 +6,9 @@ source "$(dirname $0)/../base/profile.sh"
 
 # Configuration
 ## General
-export THUIS_PROFILE_HOME="$PROFILES_HOME/thuis"
-export THUIS_PROFILE="$THUIS_PROFILE_HOME/profile.sh"
-export CURRENT_PROFILE="$THUIS_PROFILE"
+export HOME_PROFILE_HOME="$PROFILES_HOME/home"
+export HOME_PROFILE="$HOME_PROFILE_HOME/profile.sh"
+export CURRENT_PROFILE="$HOME_PROFILE"
 
 ## User
 export GIT_USER_NAME="vanpeerdevelopment"
@@ -17,7 +17,7 @@ export GIT_USER_EMAIL="dieter@vanpeerdevelopment.be"
 
 # Execution
 ## Profile
-alias reload-profile="source $THUIS_PROFILE"
+alias reload-profile="source $HOME_PROFILE"
 
 ## Git
 alias git="git -c user.name=$GIT_USER_NAME -c user.email=$GIT_USER_EMAIL"
