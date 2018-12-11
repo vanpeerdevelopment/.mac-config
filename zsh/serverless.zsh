e0453fecdb9098d8a8cc3d1e63f2e69e35ceb94c
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2018-12-11 10:01:26 +0100

    Added serverless zsh

---
+++ zsh/serverless.zsh
@@ -0,0 +1,2 @@
+[[ -f /Users/dietervp/.nvm/versions/node/v8.10.0/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.zsh ]] && . /Users/dietervp/.nvm/versions/node/v8.10.0/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.zsh
+[[ -f /Users/dietervp/.nvm/versions/node/v8.10.0/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.zsh ]] && . /Users/dietervp/.nvm/versions/node/v8.10.0/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.zsh
