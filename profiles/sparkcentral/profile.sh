3993158ea27d106493e73fdf7a2ab758eaac04e7
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2019-06-03 09:20:41 +0200

    Added databus functions

--- profiles/sparkcentral/profile.sh
+++ profiles/sparkcentral/profile.sh
@@ -41,3 +41,58 @@ function mysqldump-local() {
 function mysqlimport-local() {
     pv $3 | mysql --user=$2 -p --host=127.0.0.1 --port=3306 $1
 }
+
+## Databus
+function databus-url() {
+    local ENV="$1"
+
+    echo "http://databus.${ENV}.k8s-us-east-1"
+}
+
+function connectors() {
+    local ENV="$1"
+    local DATABUS_URL="`databus-url ${ENV}`"
+
+    curl ${DATABUS_URL}/connectors | jq
+}
+
+function connector() {
+    local ENV="$1"
+    local CONNECTOR="$2"
+    local DATABUS_URL="`databus-url ${ENV}`"
+
+    curl ${DATABUS_URL}/connectors/${CONNECTOR} | jq
+}
+
+function connector-status() {
+    local ENV="$1"
+    local CONNECTOR="$2"
+    local DATABUS_URL="`databus-url ${ENV}`"
+
+    curl ${DATABUS_URL}/connectors/${CONNECTOR}/status | jq
+}
+
+function connector-restart() {
+    local ENV="$1"
+    local CONNECTOR="$2"
+    local DATABUS_URL="`databus-url ${ENV}`"
+
+    curl -X POST ${DATABUS_URL}/connectors/${CONNECTOR}/restart | jq
+}
+
+function connector-task-restart() {
+    local ENV="$1"
+    local CONNECTOR="$2"
+    local TASK="$3"
+    local DATABUS_URL="`databus-url ${ENV}`"
+
+    curl -X POST ${DATABUS_URL}/connectors/${CONNECTOR}/tasks/${TASK}/restart | jq
+}
+
+function connector-delete() {
+    local ENV="$1"
+    local CONNECTOR="$2"
+    local DATABUS_URL="`databus-url ${ENV}`"
+
+    curl -X DELETE ${DATABUS_URL}/connectors/${CONNECTOR} | jq
+}
