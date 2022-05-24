869489a2a59fae2a94743b6ffb0ec0e703dffc77
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2022-05-24 20:40:17 +0200

    Added dv profiel

---
+++ profiles/digitaal-vlaanderen/profile.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Load base profile
+source "$(dirname $0)/../base/profile.sh"
+
+
+# Configuration
+## General
+export DIGITAAL_VLAANDEREN_PROFILE_HOME="$PROFILES_HOME/digitaal-vlaanderen"
+export DIGITAAL_VLAANDEREN_PROFILE="$DIGITAAL_VLAANDEREN_PROFILE_HOME/profile.sh"
+export CURRENT_PROFILE="$DIGITAAL_VLAANDEREN_PROFILE"
+
+## User
+export GIT_USER_NAME="dietervanpeer"
+export GIT_USER_EMAIL="dieter.vanpeer@vlaanderen.be"
+
+
+# Execution
+## Git
+alias git="git -c user.name=$GIT_USER_NAME -c user.email=$GIT_USER_EMAIL"
