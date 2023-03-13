cf54962d769ef225ce36a57ac372d683ce97e4f2
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2023-03-13 13:02:37 +0100

    Updated java and node asdf versions

--- zsh/asdf.zsh
+++ zsh/asdf.zsh
@@ -5,11 +5,11 @@ export ASDF_CONFIG_FILE="$(dirname $0)/config/.asdfrc"
 # java plugin
 asdf plugin add java https://github.com/halcyon/asdf-java.git 2>/dev/null
 source ~/.asdf/plugins/java/set-java-home.zsh
-asdf global java temurin-17.0.5+8
+asdf global java temurin-17.0.6+10
 
 # node plugin
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git 2>/dev/null
-asdf global nodejs 18.13.0
+asdf global nodejs 18.15.0
 
 # initialise completions
 fpath=(${ASDF_DIR}/completions $fpath)
