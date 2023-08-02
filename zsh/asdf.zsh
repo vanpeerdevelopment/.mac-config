5639f705f531d8dec57bbed03a95f7692d7cba43
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2023-08-02 09:58:39 +0200

    Updated default java en node versions

--- zsh/asdf.zsh
+++ zsh/asdf.zsh
@@ -5,11 +5,11 @@ export ASDF_CONFIG_FILE="$(dirname $0)/config/.asdfrc"
 # java plugin
 asdf plugin add java https://github.com/halcyon/asdf-java.git 2>/dev/null
 source ~/.asdf/plugins/java/set-java-home.zsh
-asdf global java temurin-17.0.6+10
+asdf global java temurin-17.0.8+7
 
 # node plugin
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git 2>/dev/null
-asdf global nodejs 18.15.0
+asdf global nodejs 18.17.0
 
 # python plugin
 asdf plugin add python 2>/dev/null
