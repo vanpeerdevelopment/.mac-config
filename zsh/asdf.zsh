6b0713f220d5ee921a183d6632df1a04e9170374
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2024-08-13 10:14:49 +0200

    Updated asdf versions

--- zsh/asdf.zsh
+++ zsh/asdf.zsh
@@ -3,25 +3,30 @@ source $HOME/.asdf/asdf.sh
 export ASDF_CONFIG_FILE="$(dirname $0)/config/.asdfrc"
 
 # java plugin
-asdf plugin add java https://github.com/halcyon/asdf-java.git 2>/dev/null
+asdf plugin add java https://github.com/halcyon/asdf-java.git >/dev/null
 source ~/.asdf/plugins/java/set-java-home.zsh
-asdf global java temurin-21.0.3+9.0.LTS
+asdf install java temurin-21.0.4+7.0.LTS >/dev/null
+asdf global java temurin-21.0.4+7.0.LTS
 
 # node plugin
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git 2>/dev/null
-asdf global nodejs 20.14.0
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git >/dev/null
+asdf install nodejs 20.16.0 >/dev/null
+asdf global nodejs 20.16.0
 
 # python plugin
-asdf plugin add python 2>/dev/null
-asdf global python 3.12.3
+asdf plugin add python >/dev/null
+asdf install python 3.12.5 >/dev/null
+asdf global python 3.12.5
 
 # gradle plugin
-asdf plugin-add gradle https://github.com/rfrancis/asdf-gradle.git 2>/dev/null
+asdf plugin-add gradle https://github.com/rfrancis/asdf-gradle.git >/dev/null
+asdf install gradle 8.8 >/dev/null
 asdf global gradle 8.8
 
 # pnpm plugin
-asdf plugin add pnpm 2>/dev/null
-asdf global pnpm 9.2.0
+asdf plugin add pnpm >/dev/null
+asdf install pnpm 9.7.0 >/dev/null
+asdf global pnpm 9.7.0
 
 # initialise completions
 fpath=(${ASDF_DIR}/completions $fpath)
