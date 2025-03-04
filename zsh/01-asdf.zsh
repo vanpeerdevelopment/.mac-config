a3599d80a26c4dc6f178c99bfb41a3832d46b572
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2025-03-04 10:58:06 +0100

    asdf global replaced by set

--- zsh/01-asdf.zsh
+++ zsh/01-asdf.zsh
@@ -9,24 +9,24 @@ autoload -Uz compinit && compinit
 asdf plugin add java https://github.com/halcyon/asdf-java.git >/dev/null
 source ~/.asdf/plugins/java/set-java-home.zsh >/dev/null
 asdf install java temurin-21.0.6+7.0.LTS >/dev/null
-asdf global java temurin-21.0.6+7.0.LTS >/dev/null
+asdf set --home java temurin-21.0.6+7.0.LTS >/dev/null
 
 # node plugin
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git >/dev/null
 asdf install nodejs 22.14.0 >/dev/null
-asdf global nodejs 22.14.0 >/dev/null
+asdf set --home nodejs 22.14.0 >/dev/null
 
 # pnpm plugin
 asdf plugin add pnpm >/dev/null
 asdf install pnpm 10.5.2 >/dev/null
-asdf global pnpm 10.5.2 >/dev/null
+asdf set --home pnpm 10.5.2 >/dev/null
 
 # gradle plugin
 asdf plugin add gradle https://github.com/rfrancis/asdf-gradle.git >/dev/null
 asdf install gradle 8.9 >/dev/null
-asdf global gradle 8.9 >/dev/null
+asdf set --home gradle 8.9 >/dev/null
 
 # python plugin
 asdf plugin add python >/dev/null
 asdf install python 3.13.2 >/dev/null
-asdf global python 3.13.2 >/dev/null
+asdf set --home python 3.13.2 >/dev/null
