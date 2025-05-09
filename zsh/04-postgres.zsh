106117e95f0038002ee760bd630443e597b6e31f
vanpeerdevelopment <dieter@vanpeerdevelopment.be>
2025-05-09 09:55:12 +0200

    Removed libpq brew

--- zsh/04-postgres.zsh
+++
@@ -1,3 +0,0 @@
-# sudo ln -s /opt/homebrew/opt/libpq/bin/psql /usr/local/bin/psql
-# sudo ln -s /opt/homebrew/opt/libpq/bin/pg_dump /usr/local/bin/pg_dump
-# sudo ln -s /opt/homebrew/opt/libpq/bin/pg_restore /usr/local/bin/pg_restore
